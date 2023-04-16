f66a598dc2c86b9ee914a9336ee9a02a66aecb2d
calnong <2441415772@qq.com>
2023-04-17 01:30:41 +0800

    feat(eslint-config-ts): 增加vitest

---
+++ packages/eslint-config-ts/tests/index.test.ts
@@ -0,0 +1,45 @@
+import { describe, test, expect } from 'vitest';
+import { ESLint } from 'eslint';
+
+function getText(data: string) {
+  return data;
+}
+
+const eslint = new ESLint({
+  extensions: [
+    '../index.js',
+  ],
+});
+
+// 配置正常运行
+describe('eslint-config-ts', () => {
+  test('a is assigned a value but never used.', async () => {
+    const testText = getText(`
+      const a = '';
+    `);
+    const { messages } = (await eslint.lintText(testText))[0];
+    const result = messages.find((item) => {
+      if (item.ruleId === '@typescript-eslint/no-unused-vars') {
+        return true;
+      }
+      return false;
+    });
+    const rule = '\'a\' is assigned a value but never used.';
+    expect(result?.message).toBe(rule);
+  });
+
+  test('missing semicolon.', async () => {
+    const testText = getText(`
+      const a = ''
+    `);
+    const { messages } = (await eslint.lintText(testText))[0];
+    const result = messages.find((item) => {
+      if (item.ruleId === '@typescript-eslint/semi') {
+        return true;
+      }
+      return false;
+    });
+    const rule = 'Missing semicolon.';
+    expect(result?.message).toBe(rule);
+  });
+});
