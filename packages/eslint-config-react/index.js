738e48f9b7c94329650c9aad10a8185ad7268199
calnong <2441415772@qq.com>
2023-04-28 22:01:01 +0800

    fix: 修复包依赖问题（vue&react）

--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -1,8 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { isPackageExists } = require('local-pkg');
+
+const isTs = isPackageExists('typescript');
+
 module.exports = {
   extends: [
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
-    '@fastcoder/eslint-config-ts',
+    isTs ? '@fastcoder/eslint-config-ts' : '@fastcoder/eslint-config-base',
     './libs/ant-design.js',
   ],
   rules: {
